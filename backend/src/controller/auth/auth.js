8daf90adfea316acc2c75c40c443f154b0b4a301
anandhuc1666 <anandhuc166@gmail.com>
2025-11-17 16:27:51 +0530

    whiteboard

--- backend/src/controller/auth/auth.js
+++ backend/src/controller/auth/auth.js
@@ -1,11 +1,12 @@
 import User from "../../models/authSchema.js";
 import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 //new user register form
 export const register = async (req, res) => {
   const { name, lastName, age, number, email, password } = req.body;
   try {
-    if (!name|| !age || !number || !email || !password) {
+    if (!name || !age || !number || !email || !password) {
       return res.status(404).json({ message: "please fill the form" });
     }
     const user = await User.findOne({ email });
@@ -27,9 +28,50 @@ export const register = async (req, res) => {
     await users.save();
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "internel server issue" });
+   return res.status(500).json({ message: "internel server issue" });
   }
 };
+
+export const generateOtp = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    const otp = crypto.randomInt(100000, 999999).toString();
+    user.otp = otp;
+    user.otpExpires = Date.now() + 10 * 60 * 1000;
+    await user.save();
+    await sendOtp(email, otp);
+    res.status(200).json({ message: "OTP send to email" });
+  } catch (error) {
+    console.log(error);
+   return res.status(500).json({ message: "internel server issue" });
+  }
+};
+
+export const verifyOtp = async(req,res)=>{
+  const {email,otp} = req.body;
+  try {
+    const user = await User.findOne({email})
+    if(!user){
+      return res.status(400).json({message:"user email is invalide"})
+    }
+    if(user.otp != otp || user.otpExpires <Date.now()){
+      return res.status(400).json({message:"user OTP invalide "})
+    }
+    user.otp = null
+    user.otpExpires = null
+    await user.save()
+    return res.status(200).json({message:"user OTP verifyed successfully"})
+  } catch (error) {
+      console.log(error);
+   return res.status(500).json({ message: "internel server issue" });
+  }
+}
+
+
 //find the use and jwt genarate
 
 export const login = async (req, res) => {
--- backend/src/routes/userRoute/userRoute.js
+++ backend/src/routes/userRoute/userRoute.js
@@ -1,6 +1,8 @@
 import express, { Router } from "express"
-import { register } from "../../controller/auth/auth.js"
+import { register,generateOtp, verifyOtp } from "../../controller/auth/auth.js"
 
 const route = Router()
 route.post("/register",register)
+route.post('/generateOtp',generateOtp)
+route.post('/verifyOtp',verifyOtp)
 export default route
