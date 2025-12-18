4719f146bfb52dec1c09c1bafdeb382463f1f224
anandhuc1666 <anandhuc166@gmail.com>
2025-12-18 12:29:10 +0530

    whiteboard

--- backend/src/controller/Staff/Staff.js
+++ backend/src/controller/Staff/Staff.js
@@ -1,4 +1,6 @@
 import User from "../../models/authSchema.js";
+import CSvideo from "../../models/courseVdoSchema.js";
+import { student } from "../students/students.js";
 
 const GO = `https://drive.google.com/thumbnail?id=1SfSE3pUd1n7Q2H5Cv6HMdbRpnSIlt9sh&sz=s800`;
 const FLUTTER = `https://drive.google.com/thumbnail?id=1NoPei2UHe_ZoX1HfmqTA-i0k4Rb561Ph&sz=s800`;
@@ -32,14 +34,82 @@ export const userStaff = async (req, res) => {
       staff: {
         _id: user._id,
         name: user.name,
-        email:user.email,
+        email: user.email,
         number: user.number,
-        courseId:user.course,
-        course: CourseId
+        courseId: user.course,
+        course: CourseId,
       },
     });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "internal issue" });
+  }
+};
+export const get_all_V_S = async (req, res) => {
+  try {
+    const students_list = await User.find({ role: "student" });
+    const get_video = await CSvideo.find();
+    // students ID find all student type
+    const Go_students = students_list.filter(
+      (course) => course.course === "GO"
+    );
+    const JAVA_students = students_list.filter(
+      (course) => course.course === "JAVA"
+    );
+    const PYTHON_students = students_list.filter(
+      (course) => course.course === "PYTHON"
+    );
+    const JAVASCRIPT_students = students_list.filter(
+      (course) => course.course === "JAVASCRIPT"
+    );
+    const FLUTTER_students = students_list.filter(
+      (course) => course.course === "FLUTTER"
+    );
+    // course ID, find all course type
+    const Go_videos = get_video.filter((video) => video.name === "GO");
+    const JAVA_videos = get_video.filter((video) => video.name === "JAVA");
+    const PYTHON_videos = get_video.filter((video) => video.name === "PYTHON");
+    const JAVASCRIPT_videos = get_video.filter(
+      (video) => video.name === "JAVASCRIPT"
+    );
+    const FLUTTER_videos = get_video.filter(
+      (video) => video.name === "FLUTTER"
+    );
+
+    // find the all length of the students and course type also
+    const Go_student = Go_students.length;
+    const Go_video = Go_videos.length;
+
+    const JAVA_student = JAVA_students.length;
+    const JAVA_video = JAVA_videos.length;
+
+    const PYTHON_student = PYTHON_students.length;
+    const PYTHON_video = PYTHON_videos.length;
+
+    const JAVASCRIPT_student = JAVASCRIPT_students.length;
+    const JAVASCRIPT_video = JAVASCRIPT_videos.length;
+
+    const FLUTTER_student = FLUTTER_students.length;
+    const FLUTTER_video = FLUTTER_videos.length;
+    return res.status(200).json({
+      message: "data ready to fach",
+      students: {
+        GO: Go_student,
+        JAVA: JAVA_student,
+        PYTHON: PYTHON_student,
+        JAVASCRIPT: JAVASCRIPT_student,
+        FLUTTER: FLUTTER_student,
+      },
+      video: {
+        Go: Go_video,
+        JAVA: JAVA_video,
+        PYTHON: PYTHON_video,
+        JAVASCRIPT: JAVASCRIPT_video,
+        FLUTTER: FLUTTER_video,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "internal server issue" });
   }
 };
--- backend/src/routes/staff/staffRoute.js
+++ backend/src/routes/staff/staffRoute.js
@@ -1,9 +1,10 @@
 import express, { Router } from "express"
 import verifyUser from "../../middleware/token.js";
-import { userStaff } from "../../controller/Staff/Staff.js";
+import { get_all_V_S, userStaff } from "../../controller/Staff/Staff.js";
 
 const staffRoute = Router()
 
  staffRoute.get('/staff_profile',verifyUser,userStaff)
+ staffRoute.get('/get_all_V_S',get_all_V_S)
 
 export default staffRoute
